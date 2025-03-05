5dda8deca1e86102cae30aef04e9fb08cb093c81
h0neybie <dizonabbycruz@gmail.com>
2025-03-05 22:18:10 +0800

    oki

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'featured', pathMatch: 'full' },
+  { path: 'featured', component: FeaturedComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: '**', redirectTo: 'featured' } // Wildcard route for 404
+];
